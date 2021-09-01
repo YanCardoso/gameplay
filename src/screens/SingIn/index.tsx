6bea7ec3933e725012373ce836bdf9dc1e10e9d0
Yan Cardoso <yancard7@gmail.com>
2021-09-01 17:11:17 -0300

    aula 1

---
+++ src/screens/SingIn/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { View, Text, Image, StatusBar, Button } from 'react-native';
+import { styles } from './styles';
+import IllustrationImg from '../../assets/illustration.png';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+
+export function SignIn() {
+    return (
+        <View style={styles.container}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <Image
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize{`\n`}
+                    suas jogatinhas{`\n`}
+                    facilmente{`\n`}
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games{`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+            </View>
+        </View>
+    )
+}
